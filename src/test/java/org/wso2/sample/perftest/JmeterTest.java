cbfcae933b60b006aaa08c4dc1a409870cda581a
Manuri <amaya.uom@gmail.com>
2016-07-15 15:10:00 +0530

    Initial Commit

---
+++ src/test/java/org/wso2/sample/perftest/JmeterTest.java
@@ -0,0 +1,22 @@
+package org.wso2.sample.perftest;
+
+import org.testng.annotations.Test;
+import org.wso2.automation.tools.jmeter.JMeterTest;
+import org.wso2.automation.tools.jmeter.JMeterTestManager;
+import java.io.File;
+
+public class JmeterTest {
+    @Test
+    public void runJemterTest() throws Exception {
+        JMeterTest script1 = new JMeterTest(
+                new File("/home/manurip/Documents/Work/Tasks/RnD/DIS/351release/perfTest/final/get-xml-10.jmx"));
+        JMeterTest script2 = new JMeterTest(
+                new File("/home/manurip/Documents/Work/Tasks/RnD/DIS/351release/perfTest/final/get-xml-100.jmx"));
+        JMeterTestManager manager = new JMeterTestManager();
+
+       // while(true) {
+            manager.runTest(script1);
+            manager.runTest(script2);
+        //}
+    }
+}
