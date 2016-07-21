d4b27a2107e0472ef7746a4343fb14f429e3e9c5
Manuri <amaya.uom@gmail.com>
2016-07-21 14:49:01 +0530

    Adding jmeter test running code

--- src/test/java/org/wso2/sample/perftest/JmeterTest.java
+++ src/test/java/org/wso2/sample/perftest/JmeterTest.java
@@ -1,21 +1,38 @@
 package org.wso2.sample.perftest;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.testng.annotations.Test;
 import org.wso2.automation.tools.jmeter.JMeterTest;
 import org.wso2.automation.tools.jmeter.JMeterTestManager;
 import java.io.File;
 
 public class JmeterTest {
+    private static final Log log = LogFactory.getLog(JmeterTest.class);
+
     @Test
     public void runJemterTest() throws Exception {
-        JMeterTest script1 = new JMeterTest(
-                new File("src/test/resources/jmx/getxml/get-xml-10.jmx"));
-        JMeterTest script2 = new JMeterTest(
-                new File("src/test/resources/jmx/getxml/get-xml-100.jmx"));
+
+        File folder = new File("src/test/resources/jmx");
+        File[] folders = new File[11];
+
+        int i =0 ;
+        for(File fileEntry : folder.listFiles()) {
+            if(fileEntry.isDirectory()) {
+                folders[i] = new File(fileEntry.getPath());
+            }
+            i++;
+        }
+
         JMeterTestManager manager = new JMeterTestManager();
 
-        manager.runTest(script1);
-        //manager.runTest(script2);
+        for(int j =0; j<folders.length;j++ ) {
+            for(File fileEntry : folders[j].listFiles()) {
+                if(!fileEntry.isDirectory()) {
+                    manager.runTest(new JMeterTest(new File(fileEntry.getPath())));
+                }
+            }
+        }
 
     }
 }
